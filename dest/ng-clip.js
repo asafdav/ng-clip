7fbaf474e322f2a1bcc982d008575c42ad9bffba
Asaf David <asafdav@gmail.com>
2014-02-06 06:28:53 +0200

    created a minified version

---
+++ dest/ng-clip.js
@@ -0,0 +1,54 @@
+/*jslint node: true */
+/*global ZeroClipboard */
+'use strict';
+
+angular.module('ngClipboard', []).
+  provider('ngClip', function() {
+    var self = this;
+    this.path = '//cdnjs.cloudflare.com/ajax/libs/zeroclipboard/1.3.2/ZeroClipboard.min.js';
+    return {
+      setPath: function(newPath) {
+       self.path = newPath;
+      },
+      $get: function() {
+        return {
+          path: self.path
+        };
+      }
+    };
+  }).
+  run(['$document', 'ngClip', function($document, ngClip) {
+    ZeroClipboard.config({
+      moviePath: ngClip.path,
+      trustedDomains: ["*"],
+      allowScriptAccess: "always",
+      forceHandCursor: true
+    });
+  }]).
+  directive('clipCopy', ['$window', 'ngClip', function ($window, ngClip) {
+    return {
+      scope: {
+        clipCopy: '&',
+        clipClick: '&'
+      },
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        // Create the clip object
+        var clip = new ZeroClipboard(element);
+        clip.on( 'load', function(client) {
+          var onMousedown = function (client) {
+            client.setText(scope.$eval(scope.clipCopy));
+            if (angular.isDefined(attrs.clipClick)) {
+              scope.$apply(scope.clipClick);
+            }
+          };
+          client.on('mousedown', onMousedown);
+
+          scope.$on('$destroy', function() {
+            client.off('mousedown', onMousedown);
+            client.unclip(element);
+          });
+        });
+      }
+    };
+  }]);
